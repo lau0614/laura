a6063077cc221d9ce07bb421a4a4780bddd14d4b
laura villa <99909063+lau0614@users.noreply.github.com>
2022-04-07 14:28:47 -0500

    js

---
+++ src/practica.js
@@ -0,0 +1,21 @@
+/*JavaScript como todo Lenguaje de programacion se basa en la utilizacion de palabras reservadas */
+//Creacion de Variables
+var nom = "laura";  // No se recomienda usarla 
+const nombre = "sofia";
+let apellido = "Villa";
+
+alert nombre;
+*/
+
+let numero = 3;
+let numero1 = 2;
+let suma = numero + numero1;
+print (suma);
+
+/* Operadores aritmeticos en Javascript
++ adicion 
+- resta 
+* multiplicacion 
+/ division 
+% residuo 
+*/ 
